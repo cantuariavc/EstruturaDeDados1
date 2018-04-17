1cc86678d4bf6248685ef2f6ac4556f980d7e385
Vinícius Cantuária <cantuariavc@gmail.com>
2018-04-17 18:28:42 -0300

    Adiciona estrutura do xcode

---
+++ Projeto 2/Problema 1/Problema 1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Problema 1
+//
+//  Created by Vinícius Cantuária on 17/04/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+// Mateus de Oliveira Barbosa - 14/0154817
+// Vinícius de Castro Cantuária - 14/0165169
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
