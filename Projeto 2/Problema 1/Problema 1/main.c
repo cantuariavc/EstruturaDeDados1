7617179c65707e3bfeba7e93b13975904771d0a0
Vinícius Cantuária <cantuariavc@gmail.com>
2018-04-19 22:08:19 -0300

    Adiciona geração aleatória de números
    Co-Authored-By: mateuso97 <mateuso97@users.noreply.github.com>

--- Projeto 2/Problema 1/Problema 1/main.c
+++ Projeto 2/Problema 1/Problema 1/main.c
@@ -10,9 +10,83 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void verificaAlocacao(int *vetorAlocado);
+void liberaMemoria(int *vetorAlocado);
+void geraNumerosAleatorios(int *vetorNumerosTreinamento, int *vetorNumerosTeste);
+int numeroExistente(int *vetorNumerosAleatorios, int tamanho, int numnumeroGeradoero);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE *imagem;
+    
+    int *vetorNumerosTreinamento = (int *) calloc(25, sizeof(int));
+    int *vetorNumerosTeste = (int *) calloc(25, sizeof(int));
+    
+    verificaAlocacao(vetorNumerosTreinamento);
+    verificaAlocacao(vetorNumerosTeste);
+    geraNumerosAleatorios(vetorNumerosTreinamento, vetorNumerosTeste);
+    
+    
+    liberaMemoria(vetorNumerosTreinamento);
+    liberaMemoria(vetorNumerosTeste);
+    
     return 0;
 }
+
+void verificaAlocacao(int *vetorAlocado) {
+    if (vetorAlocado == NULL) {
+        exit(1);
+    }
+}
+
+void liberaMemoria(int *vetorAlocado) {
+    free(vetorAlocado);
+}
+
+void geraNumerosAleatorios(int *vetorNumerosTreinamento, int *vetorNumerosTeste) {
+    srand((unsigned int)time(NULL));
+    
+    int *vetorNumerosAleatorios = (int *) calloc(50, sizeof(int));
+    int numeroGerado = 0;
+    
+    verificaAlocacao(vetorNumerosAleatorios);
+    
+    for (int i = 0; i < 50; i++) {
+        numeroGerado = 1 + (rand() % 50);
+        
+        while (numeroExistente(vetorNumerosAleatorios, i, numeroGerado)) {
+            numeroGerado = 1 + (rand() % 50);
+        }
+        
+        *(vetorNumerosAleatorios + i) = numeroGerado;
+    }
+    
+    for (int i = 0; i < 25; i++) {
+        *(vetorNumerosTreinamento + i) = *(vetorNumerosAleatorios + i);
+    }
+    
+    for (int i = 0; i < 25; i++) {
+        *(vetorNumerosTeste + i) = *(vetorNumerosAleatorios + (i + 25));
+    }
+    
+    liberaMemoria(vetorNumerosAleatorios);
+}
+
+int numeroExistente(int *vetorNumerosAleatorios, int tamanho, int numnumeroGeradoero) {
+    int existe = 0;
+    
+    for (int i = 0; i < tamanho; i++) {
+        if (*(vetorNumerosAleatorios + i) == numnumeroGeradoero) {
+            existe = 1;
+        }
+    }
+    
+    return existe;
+}
+
+
+
+
+
