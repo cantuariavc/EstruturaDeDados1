2a65ce0d5f306a332a71ea7bab424206ce1ab6f3
Vinícius Cantuária <cantuariavc@gmail.com>
2018-04-27 13:24:29 -0300

    Corrige laço para percorrer todas as imagens

--- Projeto 2/Problema/Problema/main.c
+++ Projeto 2/Problema/Problema/main.c
@@ -65,8 +65,10 @@ int main(int argc, const char * argv[]) {
         char *nomeImagem = alocaChar(sizeof(caminhoArquivo) + sizeof(tipoAsfalto) + sizeof(numeroArquivo));
         strcat(nomeImagem, caminhoArquivo);
 
-        int periodoAsfalto = (a < 25 || (a >= 50 && a < 75));
-        int periodoGrama = ((a >= 25 && a < 50) || a >= 75);
+        int periodoAsfaltoTreinamento = (a < 25);
+        int periodoGramaTreinamento = (a >= 25 && a < 50);
+        int periodoAsfaltoTeste = (a >= 50 && a < 75);
+        int periodoGramaTeste = (a >= 75);
         
         if (a < 25) {
             b = a;
@@ -74,30 +76,51 @@ int main(int argc, const char * argv[]) {
             b = (a - 25);
         } else if (a >= 50 && a < 75) {
             b = (a - 50);
-        } else {
+        } else if (a > 75) {
             b = (a - 75);
         }
         
-        if (periodoAsfalto) {
-            if (*(vetorNumerosTreinamento + b) <= 9) {
+        int numeroUmDigitoTreinamento = (*(vetorNumerosTreinamento + b) <= 9);
+        int numeroDoisDigitosTreinamento = (*(vetorNumerosTreinamento + b) > 9);
+        int numeroUmDigitoTeste = (*(vetorNumerosTeste + b) <= 9);
+        int numeroDoisDigitosTeste = (*(vetorNumerosTeste + b) > 9);
+
+        if (periodoAsfaltoTreinamento) {
+            if (numeroUmDigitoTreinamento) {
                 sprintf(numeroArquivo, "%c%d%s", '0', *(vetorNumerosTreinamento + b), ".txt\0");
-            } else if (*(vetorNumerosTreinamento + b) > 9) {
+            } else if (numeroDoisDigitosTreinamento) {
                 sprintf(numeroArquivo, "%d%s", *(vetorNumerosTreinamento + b), ".txt\0");
             }
-            
-            strcat(nomeImagem, tipoAsfalto);
-            strcat(nomeImagem, numeroArquivo);
-        } else if (periodoGrama) {
-            if (*(vetorNumerosTeste + b) <= 9) {
+        } else if (periodoGramaTreinamento) {
+            if (numeroUmDigitoTreinamento) {
+                sprintf(numeroArquivo, "%c%d%s", '0', *(vetorNumerosTreinamento + b), ".txt\0");
+            } else if (numeroDoisDigitosTreinamento) {
+                sprintf(numeroArquivo, "%d%s", *(vetorNumerosTreinamento + b), ".txt\0");
+            }
+        } else if (periodoAsfaltoTeste) {
+            if (numeroUmDigitoTeste) {
                 sprintf(numeroArquivo, "%c%d%s", '0', *(vetorNumerosTeste + b), ".txt\0");
-            } else if (*(vetorNumerosTeste + b) > 9) {
+            } else if (numeroDoisDigitosTeste) {
                 sprintf(numeroArquivo, "%d%s", *(vetorNumerosTeste + b), ".txt\0");
             }
-            
+        } else if (periodoGramaTeste) {
+            if (numeroUmDigitoTeste) {
+                sprintf(numeroArquivo, "%c%d%s", '0', *(vetorNumerosTeste + b), ".txt\0");
+            } else if (numeroDoisDigitosTeste) {
+                sprintf(numeroArquivo, "%d%s", *(vetorNumerosTeste + b), ".txt\0");
+            }
+        }
+        
+        if (periodoAsfaltoTreinamento || periodoAsfaltoTeste) {
+            strcat(nomeImagem, tipoAsfalto);
+            strcat(nomeImagem, numeroArquivo);
+        } else if (periodoGramaTreinamento || periodoGramaTeste) {
             strcat(nomeImagem, tipoGrama);
             strcat(nomeImagem, numeroArquivo);
         }
-            
+        
+        printf("%s\n\n", nomeImagem);
+        
         FILE *imagemTexto = fopen(nomeImagem, "r");
         verificaAlocacaoArquivo(imagemTexto);
         free(nomeImagem);
@@ -115,9 +138,9 @@ int main(int argc, const char * argv[]) {
         int *frequenciaILBP = alocaInt(512);
         calculaVizinhancasOito(imagemPrograma, quantidadeLinhas, quantidadeColunas, frequenciaILBP);
         
-        if (periodoAsfalto) {
+        if (periodoAsfaltoTreinamento) {
             calculaMediaILBPAsfalto(frequenciaILBPAsfalto, frequenciaILBP);
-        } else if (periodoGrama) {
+        } else if (periodoGramaTreinamento) {
             calculaMediaILBPGrama(frequenciaILBPGrama, frequenciaILBP);
         }
         free(frequenciaILBP);
