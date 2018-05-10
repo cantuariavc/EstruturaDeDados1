cd4cb90a79c293267c4a0521ec5440ed2ae2da99
Vinícius Cantuária <cantuariavc@gmail.com>
2018-05-09 22:59:11 -0300

    Adiciona impressão do menu

---
+++ Exercícios/Lista Duplamente Encadeada/src/servicos.c
@@ -0,0 +1,25 @@
+//
+//  servicos.c
+//  Lista Duplamente Encadeada
+//
+//  Created by Vinícius Cantuária on 09/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#include "servicos.h"
+
+void imprimeMenu(void) {
+    printf("Menu");
+    printf("\n");
+    printf("1 - Inserir nó na lista");
+    printf("\n");
+    printf("2 - Imprimir lista");
+    printf("\n");
+    printf("3 - Excluir nó");
+    printf("\n");
+    printf("4 - Excluir lista");
+    printf("\n");
+    printf("0 - Sair");
+    printf("\n");
+    printf("Opção: ");
+}
---
+++ Exercícios/Lista Duplamente Encadeada/src/servicos.h
@@ -0,0 +1,16 @@
+//
+//  servicos.h
+//  Lista Duplamente Encadeada
+//
+//  Created by Vinícius Cantuária on 09/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#ifndef servicos_h
+#define servicos_h
+
+#include <stdio.h>
+
+void imprimeMenu(void);
+
+#endif /* servicos_h */
