3c30f12fc9027e8e80b17b8421e91831713c1dd7
Vinícius Cantuária <cantuariavc@gmail.com>
2018-05-29 22:41:29 -0300

    Adiciona estrutura do voo

---
+++ Projeto 4/src/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Projeto 4
+//
+//  Created by Vinícius Cantuária on 29/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
---
+++ Projeto 4/src/voo.h
@@ -0,0 +1,21 @@
+//
+//  voo.h
+//  Projeto 4
+//
+//  Created by Vinícius Cantuária on 29/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#ifndef voo_h
+#define voo_h
+
+#include <stdio.h>
+
+typedef struct voo {
+    char codigo[7];
+    char status;
+    int quantidadeDeCombustivel;
+    struct voo *proximo;
+} Voo;
+
+#endif /* voo_h */
