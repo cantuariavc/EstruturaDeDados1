72fc4c224eeb09c30211558efd600faa65b039bd
Vinícius Cantuária <cantuariavc@gmail.com>
2018-05-30 00:19:11 -0300

    Adiciona configuração da hora inicial

---
+++ Projeto 4/src/tempo.c
@@ -0,0 +1,15 @@
+//
+//  tempo.c
+//  Projeto 4
+//
+//  Created by Vinícius Cantuária on 29/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#include "tempo.h"
+
+int tempoInicial() {
+    int horaInicial = 8;
+    
+    return horaInicial;
+}
---
+++ Projeto 4/src/tempo.h
@@ -0,0 +1,14 @@
+//
+//  tempo.h
+//  Projeto 4
+//
+//  Created by Vinícius Cantuária on 29/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#ifndef tempo_h
+#define tempo_h
+
+int tempoInicial(void);
+
+#endif /* tempo_h */
