d1f1d699b30977d4045f1f355e62c84b9f7a0a6e
Vinícius Cantuária <cantuariavc@gmail.com>
2018-05-29 23:16:34 -0300

    Adiciona função para gerar número aleatório

---
+++ Projeto 4/src/numerosAleatorios.c
@@ -0,0 +1,17 @@
+//
+//  numerosAleatorios.c
+//  Projeto 4
+//
+//  Created by Vinícius Cantuária on 29/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#include "numerosAleatorios.h"
+
+int geraNumeroAleatorio(int tamanhoMinimo, int tamanhoMaximo) {
+    srand((unsigned int)time(NULL));
+    
+    int numeroGerado = tamanhoMinimo + (rand() % tamanhoMaximo);
+    
+    return numeroGerado;
+}
---
+++ Projeto 4/src/numerosAleatorios.h
@@ -0,0 +1,17 @@
+//
+//  numerosAleatorios.h
+//  Projeto 4
+//
+//  Created by Vinícius Cantuária on 29/05/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+
+#ifndef numerosAleatorios_h
+#define numerosAleatorios_h
+
+#include <stdlib.h>
+#include <time.h>
+
+int geraNumeroAleatorio(int tamanhoMinimo, int tamanhoMaximo);
+
+#endif /* numerosAleatorios_h */
