862b1174c361a9b8897db6c567ba8572cfe02545
Vinícius Cantuária <cantuariavc@gmail.com>
2018-06-01 12:22:07 -0300

    Adiciona geração da fila de aproximações na main

--- Projeto 4/src/main.c
+++ Projeto 4/src/main.c
@@ -6,9 +6,16 @@
 //  Copyright © 2018 Vinícius Cantuária. All rights reserved.
 //
 
-#include <stdio.h>
+#include "fila.h"
+#include "numeroAleatorio.h"
+#include "voo.h"
 
 int main(int argc, const char * argv[]) {
+    int tamanhoVetorAproximacoes = geraNumeroAleatorio(QUANTIDADEMINIMAAPROXIMACOES, QUANTIDADEMAXIMAAPROXIMACOES);
+    int *vetorComNumerosAleatorios = geraVetorComNumerosAleatorios(tamanhoVetorAproximacoes);
+    Fila *filaAproximacoes = geraFilaAproximacoes(tamanhoVetorAproximacoes, vetorComNumerosAleatorios);
+
+    desalocaFila(filaAproximacoes);
     
     return 0;
 }
