121f23aa7fc45ac50cfc55fd4a8660f9321733c3
Vinícius Cantuária <cantuariavc@gmail.com>
2018-06-05 13:52:17 -0300

    Adiciona impressao.c para compilação

--- Projeto 4/run.sh
+++ Projeto 4/run.sh
@@ -1,1 +1,1 @@
-cd src/ && gcc main.c fila.c memoria.c numeroAleatorio.c tempo.c voo.c -o main.out && ./main.out
+cd src/ && gcc main.c fila.c impressao.c memoria.c numeroAleatorio.c tempo.c voo.c -o main.out && ./main.out
