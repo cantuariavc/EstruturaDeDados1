adb8748b3c4a0510616e90311449b8cdf5fb3d33
Vinícius Cantuária <cantuariavc@gmail.com>
2018-06-13 00:31:53 -0300

    Adiciona main

---
+++ Projeto 5/src/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Projeto 5
+//
+//  Created by Vinícius Cantuária on 12/06/2018.
+//  Copyright © 2018 Vinícius Cantuária. All rights reserved.
+//
+// Mateus de Oliveira Barbosa - 14/0154817
+// Vinícius de Castro Cantuária - 14/0165169
+//
+
+#include "arvore.h"
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
