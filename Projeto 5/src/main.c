b2115f930c7057ddadde00f1d771ab7e81b8c915
Vinícius Cantuária <cantuariavc@gmail.com>
2018-06-13 12:32:03 -0300

    Adiciona protótipo de escolha a partir do menu

--- Projeto 5/src/main.c
+++ Projeto 5/src/main.c
@@ -9,9 +9,55 @@
 // Vinícius de Castro Cantuária - 14/0165169
 //
 
+#include <string.h>
+
 #include "arvore.h"
+#include "servicos.h"
 
 int main(int argc, const char * argv[]) {
+    int opcao = 11;
+    No *raiz = NULL;
+    
+    do {
+        do {
+            imprimeMenu(&opcao);
+        } while (opcao < 0 || opcao > 10);
+        
+        switch (opcao) {
+            case 1:
+//                raiz = loadTreeFromFile(arquivo);
+                break;
+            case 2:
+//                showTree(raiz);
+                break;
+            case 3:
+//                isFull(raiz);
+                break;
+            case 4:
+//                searchValue(raiz);
+                break;
+            case 5:
+//                getHeight(raiz);
+                break;
+            case 6:
+//                removeValue(&raiz, valor);
+                break;
+            case 7:
+//                printInOrder(raiz);
+                break;
+            case 8:
+//                printPreOrder(raiz);
+                break;
+            case 9:
+//                printPostOrder(raiz);
+                break;
+            case 10:
+//                balanceTree(&raiz);
+                break;
+            default:
+                break;
+        }
+    } while (opcao != 0);
     
     return 0;
 }
